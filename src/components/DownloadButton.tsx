345f1f024a42ca2c62e18422894a5444dfeeea33
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-02 04:02:56 -0700

    download file

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
 
-const DownloadButton = ({ fileLink }) => {
-  return <div>DownloadButton</div>;
+const DownloadButton = ({ fileLink }: { fileLink: string }) => {
+  const handleDownload = () => {
+    window.open(fileLink, "_blank");
+  };
+  return (
+    <button
+      onClick={handleDownload}
+      className="bg-[#0096E1] px-3 py-2 w-36  text-center rounded-2xl cursor-pointer font-bold mt-10 hover:bg-[#007ce1]"
+    >
+      Download
+    </button>
+  );
 };
 
 export default DownloadButton;

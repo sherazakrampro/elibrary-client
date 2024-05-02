6e7b31757ff2479c2b54b778e65a9e59ab5060d9
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-02 04:15:56 -0700

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import BookList from "@/components/BookList";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 const Home = async () => {
   return (
     <div className="max-w-6xl mx-auto mt-10">
       <h1 className="text-5xl font-bold mb-10">Welcome to eLibrary</h1>
-      <BookList />
+      <Suspense fallback={<Loading />}>
+        <BookList />
+      </Suspense>
     </div>
   );
 };
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="text-2xl mx-auto min-h-[800px] px-5 pb-10 mb-6 max-w-6xl">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
